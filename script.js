549ba886a702391ba67a136930f7ef8372b5915d
Melissard02 <melissa.rd1258@gmail.com>
2026-03-03 14:57:07 -0600

    I don't know what I did but it fixed itself???

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
+// script.js
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-
 async function loadAPOD() {
     try {
+
         const response = await fetch(
             `https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`
-        )
+        );
+
         const data = await response.json();
+        console.log(data.url);
+        console.log(data.media_type);
+
+        const heroMedia = document.getElementById("hero-media");
+        heroMedia.innerHTML = "";
+
         if (data.media_type === "image") {
-            document.getElementById("hero").style.backgroundImage = `url(${data.url})`;
-            document.getElementById("apod-title").textContent = data.title;
-            document.getElementById("apod-description").textContent = data.explanation;
+
+            heroMedia.innerHTML = `
+                <img src="${data.url}" alt="${data.title}">
+            `;
+
+        } else if (data.media_type === "video") {
+
+            heroMedia.innerHTML = `
+                <video autoplay muted loop playsinline>
+                    <source src="${data.url}" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            `;
+
         }
+
+        document.getElementById("apod-title").textContent = data.title;
+        document.getElementById("apod-description").textContent =
+            data.explanation.slice(0, 250) + "...";
+
     } catch (error) {
         console.error("Error loading APOD", error);
-    };
+    }
 }
 
 async function loadMarsPhotos() {
