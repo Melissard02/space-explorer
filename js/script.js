c635765e1ed3c8da552e5182fa6ab289b0212e18
Melissard02 <melissa.rd1258@gmail.com>
2026-03-13 15:34:36 -0500

    Added new features to index

--- js/script.js
+++ js/script.js
@@ -5,10 +5,33 @@ import { loadNearEarth } from "./asteroid.js";
 
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
 
+async function loadFeaturedImage() {
+
+    const container = document.getElementById("featured-container");
+
+    const response = await fetch(
+        "https://images-api.nasa.gov/search?q=galaxy&media_type=image"
+    );
+
+    const data = await response.json();
+
+    const item = data.collection.items[0];
+
+    container.innerHTML = `
+        <div class="card">
+            <img src="${item.links[0].href}">
+            <p>${item.data[0].title}</p>
+        </div>
+    `;
+}
+
+
+
 function init() {
     loadAPOD(API_KEY);
     loadMarsPhotos(API_KEY);
     loadNearEarth(API_KEY);
+    loadFeaturedImage();
 }
 
 window.onload = init;
