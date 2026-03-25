c8e4a66e7638a050f37d36256189f1d5a5cf4494
Melissard02 <melissa.rd1258@gmail.com>
2026-03-25 16:42:36 -0500

    Deleted unused code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,6 @@
 // script.js
 import { loadAPOD } from "./apodHero.js";
-
-
 const API_KEY = import.meta.env.VITE_NASA_API_KEY;
-
-// async function loadFeaturedImage() {
-
-//     const container = document.getElementById("featured-container");
-
-//     const response = await fetch(
-//         "https://images-api.nasa.gov/search?q=galaxy&media_type=image"
-//     );
-
-//     const data = await response.json();
-
-//     const item = data.collection.items[0];
-
-//     container.innerHTML = `
-//         <div class="card">
-//             <img src="${item.links[0].href}">
-//             <p>${item.data[0].title}</p>
-//         </div>
-//     `;
-// }
 
 async function loadRandomAPOD(API_KEY, count = 8) {
     const container = document.getElementById("random-apod");
@@ -70,11 +48,14 @@ async function loadRandomAPOD(API_KEY, count = 8) {
     });
 }
 
+async function randomFacts() {
+    
+}
+
 
 
 function init() {
     loadAPOD(API_KEY);
-    // loadFeaturedImage();
     loadRandomAPOD(API_KEY);
 }
 
